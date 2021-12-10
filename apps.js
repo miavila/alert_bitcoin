542ad69b547aca47eea13fd76bbd51b621a7582b
miavila <miavila@me.com>
2021-12-10 12:51:47 +0100

    Renombrar claves

--- apps.js
+++ apps.js
@@ -1,5 +1,5 @@
 let ws = new WebSocket('wss://stream.binance.com:9443/ws/btceur@trade');
-let stockPriceElement = document.getElementById('stock-price');
+let stockPriceElement = document.getElementById('cryptoPrice');
 let lastPrice =null;
 
 function sendMail (lastPrice){
@@ -8,20 +8,20 @@ function sendMail (lastPrice){
         to_name:'mia',
         message:lastPrice
     };
-    emailjs.send('service_oiyosvf', 'template_wf87xfm', tempParams)
+    emailjs.send('service_oiyosvf', 'Template ID', tempParams)
     .then(function(res){
         console.log("success", res.status);
     });
-
 }
 
 ws.onmessage =(event) => {
+    let precioAlert = 50000
     let sotckObject = JSON.parse(event.data);
     let price = parseFloat(sotckObject.p).toFixed(2);
     stockPriceElement.innerText = price;
     stockPriceElement.style.color = !lastPrice || lastPrice === price ? 'black' : price > lastPrice ? 'green' : 'red';
     lastPrice = price;
-    if (lastPrice < 51800){
+    if (lastPrice < precioAlert){
         ws.close();
         sendMail(lastPrice);
         alert("El precio del Bitcoins es de: " + lastPrice + " ¿Buen precio para comprar?");
